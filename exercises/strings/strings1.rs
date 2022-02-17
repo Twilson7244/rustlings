17bfea7085dbe2ac504846fd5ba3c74d2241b270
Twilson7244 <99853853+Twilson7244@users.noreply.github.com>
2022-02-16 20:43:52 -0600

    Update strings1.rs

--- exercises/strings/strings1.rs
+++ exercises/strings/strings1.rs
@@ -3,7 +3,14 @@
 // Execute `rustlings hint strings1` for hints ;)
 
 // I AM NOT DONE
+@@ -0,0 +1,7 @@
+root = true
 
+[*.rs]
+end_of_line = lf
+insert_final_newfile = true
+indent_style = space
+indent_size = 4
 fn main() {
     let answer = current_favorite_color();
     println!("My current favorite color is {}", answer);
